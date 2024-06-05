d94503f2e7df64b9e8b9340c1696ee98d9859e61
danielcobgue <d.cobo@fp.safavalladolid.com>
2024-06-05 13:33:17 +0200

    ejercicio 1.5

--- Lacteos/src/ejercicio/Queso.java
+++ Lacteos/src/ejercicio/Queso.java
@@ -18,9 +18,13 @@ public class Queso {
 		System.out.println("QUESOS ENTREPINARES");
 		System.out.println("Registro Sanitario No 52/38751");
 		System.out.println("Para consultar el lote del producto revise la etiqueta");
-		System.out.println("Peso: " + this.peso); System.out.println("Tipo de leche: " +
-		this.tipo_leche);
-		System.out.println("Cantidad de leche: " + this.cantidad_leche); }
+		imprimir_detalle(); }
+	
+	public void imprimir_detalle() {
+		System.out.println("Peso: " + this.peso);
+		System.out.println("Tipo de leche: " +this.tipo_leche);
+		System.out.println("Cantidad de leche: " + this.cantidad_leche);
+	}
 	
 	String getTipo_leche() {
 		return tipo_leche;
