4bb382a2d989df268e4c86de655d499624ca41d8
danielcobgue <d.cobo@fp.safavalladolid.com>
2024-06-05 13:23:35 +0200

    ejercicio 1.3

--- Lacteos/src/ejercicio/Queso.java
+++ Lacteos/src/ejercicio/Queso.java
@@ -2,7 +2,34 @@ package ejercicio;
 
 public class Queso {
 	
-	String tipo_leche;
-	int cantidad_leche;
-	double peso;
+	private String tipo_leche;
+	private int cantidad_leche;
+	private double peso;
+	public Queso() {
+		
+	}
+	public Queso(String tipo_leche, int cantidad_leche, double peso) {
+		this.setTipo_leche(tipo_leche);
+		this.setCantidad_leche(cantidad_leche);
+		this.setPeso(peso);
+	}
+	String getTipo_leche() {
+		return tipo_leche;
+	}
+	void setTipo_leche(String tipo_leche) {
+		this.tipo_leche = tipo_leche;
+	}
+	int getCantidad_leche() {
+		return cantidad_leche;
+	}
+	void setCantidad_leche(int cantidad_leche) {
+		this.cantidad_leche = cantidad_leche;
+	}
+	double getPeso() {
+		return peso;
+	}
+	void setPeso(double peso) {
+		this.peso = peso;
+	}
+	
 }
